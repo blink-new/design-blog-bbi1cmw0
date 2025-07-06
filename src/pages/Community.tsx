08a4a349385eeb514f1a573a9a8973e095218537
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-07-06 20:03:50 +0000

    Fix runtime error: corrected Community.tsx import of useState from React instead of react-router-dom

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link, useNavigate, useState } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from '../components/ui/avatar';
